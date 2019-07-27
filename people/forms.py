d382bd5e9640f4116da65cef66928835e88d63b1
Sina Rismanchian <sina.rismanchian@gmail.com>
2019-07-27 11:27:16 +0430

    finalizing.

--- people/forms.py
+++ people/forms.py
@@ -1,3 +1,4 @@
+import requests
 from django import forms
 from people.models import *
 
@@ -5,16 +6,20 @@ from people.models import *
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = StudentApplication
-        fields = ('first_name', 'last_name', 'school_name', 'city', 'email', 'answer', 'grade', 'city_wanted')
+        fields = ('first_name', 'last_name', 'phone_number', 'email', 'school_name', 'city', 'answer', 'grade',
+                  'city_wanted', 'request_dorm', 'description')
         labels = {
             'first_name': 'نام',
             'last_name': 'نام خانوادگی',
             'school_name': 'نام مدرسه',
-            'city': 'شهر',
+            'city': 'شهر محل سکونت',
             'email': 'ایمیل',
+            'phone_number': 'شماره تلفن همراه',
             'answer': 'پاسخ به سوالات',
             'grade': 'پایه',
-            'city_wanted': 'شهری که می‌خواهید در کلاس‌هایش شرکت کنید'
+            'city_wanted': 'مایل به شرکت در مدرسهٔ کدام شهر هستید؟',
+            'request_dorm': 'درخواست خوابگاه دارم.',
+            'description': 'توضیحات بیشتر'
         }
         label_suffix = ''
 
