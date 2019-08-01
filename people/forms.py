93eab36df9d4695eb39ee1523ac2c39defc4fcc4
Sina Rismanchian <sina.rismanchian@gmail.com>
2019-08-01 18:13:33 +0430

    change in form.

--- people/forms.py
+++ people/forms.py
@@ -4,12 +4,12 @@ from captcha.fields import CaptchaField
 
 
 class RegistrationForm(forms.ModelForm):
-    captcha = CaptchaField()
+    captcha = CaptchaField(label='حروف تصویر زیر را وارد کنید.')
 
     class Meta:
         model = StudentApplication
         fields = ('first_name', 'last_name', 'phone_number', 'email', 'school_name', 'city', 'answer', 'grade',
-                  'city_wanted', 'request_dorm', 'description')
+                  'city_wanted', 'second_choice_available', 'request_dorm', 'description')
         labels = {
             'first_name': 'نام',
             'last_name': 'نام خانوادگی',
@@ -19,9 +19,9 @@ class RegistrationForm(forms.ModelForm):
             'phone_number': 'شماره تلفن همراه',
             'answer': 'پاسخ به سوالات',
             'grade': 'پایه',
-            'city_wanted': 'مایل به شرکت در مدرسهٔ کدام شهر هستید؟',
+            'city_wanted': 'کدام شهر اولویت اول شما برای حضور در مدرسه است؟',
+            'second_choice_available':  'امکان‌پذیری اولویت دوم',
             'request_dorm': 'درخواست خوابگاه دارم.',
             'description': 'توضیحات بیشتر'
         }
         label_suffix = ''
-
